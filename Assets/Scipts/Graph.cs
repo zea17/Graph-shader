215efde1755fd3b7ca5e3ceaaa7078efa4b31f4e
zea <zeaak17@gmail.com>
2025-08-03 16:17:28 +0330

    Sum of two sine waves.

--- Assets/Scipts/Graph.cs
+++ Assets/Scipts/Graph.cs
@@ -1,10 +1,14 @@
-// Your Graph.cs (unchanged from previous correction)
 using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    [SerializeField] Transform pointPrefab;
-    [SerializeField, Range(10, 100)] int resolution = 10;
+
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
     Transform[] points;
 
     void Awake()
@@ -12,16 +16,14 @@ public class Graph : MonoBehaviour
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-
         points = new Transform[resolution];
         for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab);
-            point.SetParent(transform, false);
+            Transform point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
-            points[i] = point;
+            point.SetParent(transform, false);
         }
     }
 
@@ -32,7 +34,7 @@ public class Graph : MonoBehaviour
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            position.y = FunctionLibrary.MultiWave(position.x, time);
             point.localPosition = position;
         }
     }
